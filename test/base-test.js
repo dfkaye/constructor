c986d5a34ca40d9eb81e305cc7b34eadcea21ba4
david kaye <dfkaye@yahoo.com>
2013-03-03 17:20:00 -0800

    base test

---
+++ test/base-test.js
@@ -0,0 +1,28 @@
+﻿var test = require('tape');
+var Constructor = require('../src/Constructor.js').Constructor;
+
+// test base object
+function Base() {};
+
+test('should return function without new keyword', function (t) {
+  t.plan(1);
+  t.strictEqual(Constructor(Base), Base);
+});
+
+test('should return function using new keyword', function (t) {
+  t.plan(1);
+  t.strictEqual(new Constructor(Base), Base);
+});
+
+test('should return function', function (t) {
+  t.plan(1);
+  t.strictEqual(1, 0);
+});
+
+/* template */
+/*
+test('$1 should return $2', function (t) {
+  t.plan(1);
+  t.fail('next test to implement');
+});
+*/
