4ffb3f06678ac0877daa5d5f8d9fd8ae97e36cbf
david kaye <dfkaye@yahoo.com>
2013-03-04 09:34:10 -0800

    base antipattern test

--- test/base/base.js
+++ test/base/base.js
@@ -31,6 +31,54 @@ test('should return Base as constructor', function (t) {
     }), Base);
 });
 
+
+test('reuse constructor', function (t) {
+  t.plan(1);
+  var A = Constructor({
+    constructor: Base
+  });
+  
+  t.strictEqual(Constructor(A), Base);
+});
+
+test('reuse constructor test() method', function (t) {
+  t.plan(1);
+  var msg = 'test string';
+  var A = new Constructor({
+    constructor: Base,
+    test: function () {
+        return msg;
+    }
+  });
+  
+  var a = new A();
+  t.strictEqual(a.test(), msg);
+});
+
+/* this reuse pattern is the case for the extend() method */
+test('ANTI-PATTERN: wipeout constructor test() method with reuse call', function (t) {
+  t.plan(1);
+  var msg = 'test string';
+  var A = new Constructor({
+    constructor: Base,
+    test: function () {
+        return msg;
+    }
+  });
+  
+  var B = new Constructor({
+    constructor: A
+  });
+  
+  var a = new A();
+  try {
+    t.strictEqual(a.test(), msg);
+    t.fail('should fail - test() should have been removed from A');
+  } catch (e) {
+    t.strictEqual(typeof a.test, 'undefined');
+  }
+});
+
 /* template */
 /*
 test('$1 should return $2', function (t) {
