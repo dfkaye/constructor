8446a773343adfb42a2c3e71c788a3c54d2a3b4e
david kaye <dfkaye@yahoo.com>
2013-03-03 17:10:33 -0800

    add test file

---
+++ test/Constructor-test.js
@@ -0,0 +1,9 @@
+﻿var test = require('tape');
+var normalize = require('../src/path-normalize.js').normalize;
+
+/* template */
+
+test('$1 should return $2', function (t) {
+  t.plan(1);
+  t.fail('next test to implement');
+});
